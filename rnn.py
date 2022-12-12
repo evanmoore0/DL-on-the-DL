a92010b95312a0777c405f7bdc5d645f15a9e866
felicityhade <78045179+felicityhade@users.noreply.github.com>
2022-12-11 23:51:34 -0500

    final rnn

--- rnn.py
+++ rnn.py
@@ -21,30 +21,21 @@ class MyRNN(tf.keras.Model):
         self.count = 0
 
         self.vocab_size = vocab_size
-        self.rnn_size = rnn_size # RNN size = 22 from paper (I think) -- note that this doesn't actually get used...
+        self.rnn_size = rnn_size
         self.embed_size = embed_size  # Embed size = 128
-
-        ## Define an embedding component to embed the word indices into a trainable embedding space.
-        ## Define a recurrent component to reason with the sequence of data. 
-        ## You may also want a dense layer near the end...    
+   
         self.input_layer = tf.keras.layers.InputLayer()
-        self.embedding = tf.keras.layers.Embedding(vocab_size, embed_size) # Input (None, 22) -> Output (None, 22, 128)
-        # self.dropout = tf.keras.layers.SpatialDropout1D(0.2)
+       
+        self.embedding = tf.keras.layers.Embedding(1000, embed_size) # Input (None, 22) -> Output (None, 22, 128)
 
         # new_steps = (steps - kernel_size) / stride + 1
         # kernel_size = steps - (new_steps - 1) * stride  = 22 - (18 - 1) * 1 = 5
-
-        # self.conv1d = tf.keras.layers.Conv1D(64, 5, activation='relu')
+       
         # for padding = 'same', we have output_size = input_size / stride --> stride = 3 --> pool_size = 3
-        self.maxpooling = tf.keras.layers.MaxPooling1D(pool_size=3, padding='same')
-        # self.bidir_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64))
-        # self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
 
-        # self.input_layer = tf.keras.layers.InputLayer(input_shape=(100,))
-        # self.embedding = tf.keras.layers.Embedding(input_dim=1000, output_dim=100, input_length=100)
+        self.maxpooling = tf.keras.layers.MaxPooling1D(pool_size=3, padding='same')
         self.dropout = tf.keras.layers.SpatialDropout1D(0.2)
         self.conv1d = tf.keras.layers.Conv1D(64, 5, activation='relu')
-        # self.maxpooling = tf.keras.layers.MaxPooling1D(pool_size=4)
         self.bidir_lstm = tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64))
         self.dense = tf.keras.layers.Dense(1, activation='sigmoid')
 
@@ -53,20 +44,20 @@ class MyRNN(tf.keras.Model):
 
     def call(self, inputs):
         """
-        Based entirely upon the paper
+        Based upon the paper
         """
+        # Note: shapes below are from the paper and don't match ours exactly
 
         # Input Layer takes (None, 22) -> (None, 22):
         inputs = self.input_layer(inputs)
-        # print(inputs.shape)
         # Embedding Layer takes (None, 22) -> (None, 22, 128)
         inputs = self.embedding(inputs)
-        # Apply SpatialDropout1D (None, 22, 128) -> (None, 22, 128)):
+        # Apply SpatialDropout1D (None, 22, 128) -> (None, 22, 128):
         inputs = self.dropout(inputs)
         # Apply Conv1D to yield output shape (None, 18, 64):
         inputs = self.conv1d(inputs)
         # Add batch normalization (following convolution) in an attempt to improve original model:
-        inputs = self.batch_norm(inputs)
+        # inputs = self.batch_norm(inputs)
         # Apply MaxPooling1D to yield output shape (None, 6, 64):
         inputs = self.maxpooling(inputs)
         # Apply Bidirectional LSTM to yield output shape (None, 392):
@@ -96,22 +87,9 @@ def get_text_model(vocab_sz):
     
     model = MyRNN(vocab_sz)
 
-    class PerplexityMetric(tf.keras.metrics.Metric):
-        def __init__(self, name='perplexity', **kwargs):
-            super().__init__(name=name, **kwargs)
-            self.cross_entropy = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True, reduction='none')
-            self.perplexity = self.add_weight(name='self perplexity', initializer='zeros')
-        def update_state(self, y_true, y_pred, sample_weight=None):
-            loss = self.cross_entropy(y_true, y_pred)
-            self.perplexity = tf.exp(tf.reduce_mean(loss))
-        def result(self):
-            return self.perplexity
-        def reset_states(self):
-            self.perplexity = 0.0
-
     ## Consider changing the loss and accuracy metrics...
     loss_metric = 'binary_crossentropy'
-    acc_metric  = 'accuracy' # PerplexityMetric()
+    acc_metric  = 'accuracy'
 
     model.compile(
         optimizer='adam',
@@ -133,41 +111,14 @@ def main():
 
     args.model.fit(
         padded_train_sequence, y_train,
-        epochs=args.epochs, 
+        epochs=5, 
         batch_size=args.batch_size,
         validation_data=(padded_test_sequence, y_test)
     )
 
     ## Test model on a single example:
-    # example_tweet = 'I LOVE deep learning! I\'ve learned so much in this class.'
+    # example_tweet = 'I love deep learning! I\'ve learned so much in this class.'
     # args.model.classify_tweet(example_tweet, vocab)
-
-    # embed_size = 128
-    # model = tf.keras.models.Sequential([
-    #     tf.keras.layers.InputLayer(input_shape=(22,)),
-    #     tf.keras.layers.Embedding(vocab_sz, embed_size), # Input (None, 22) -> Output (None, 22, 128)
-    #     tf.keras.layers.SpatialDropout1D(0.2),
-    #     tf.keras.layers.Conv1D(64, 5, activation='relu'),
-    #     tf.keras.layers.BatchNormalization(),
-    #     tf.keras.layers.MaxPooling1D(pool_size=3),
-    #     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-    #     tf.keras.layers.Dense(1, activation='sigmoid')
-    # ])
-
-    # model = tf.keras.models.Sequential([
-    #     tf.keras.layers.InputLayer(input_shape=(100,)),
-    #     tf.keras.layers.Embedding(input_dim=1000, output_dim=100, input_length=100),
-    #     tf.keras.layers.SpatialDropout1D(0.2),
-    #     tf.keras.layers.Conv1D(64, 5, activation='relu'),
-    #     tf.keras.layers.MaxPooling1D(pool_size=4),
-    #     tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(64)),
-    #     tf.keras.layers.Dense(1, activation='sigmoid')
-    # ])
-
-#     model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-
-# # Fit the model
-#     history = model.fit(padded_train_sequence, y_train, epochs=5, batch_size = 100, validation_data=(padded_test_sequence, y_test))
 
 if __name__ == '__main__':
     main()
